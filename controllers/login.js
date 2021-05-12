c33675216d3f96cdabeccdeb0da78a7c33de174d
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2021-05-12 14:37:13 -0300

    first commit

---
+++ controllers/login.js
@@ -0,0 +1,3 @@
+module.exports = app => {
+    app.get('/uppersoft', (req, res) => res.send('teste'))
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const consign = require('consign');
+
+const app = express()
+
+consign()
+    .include('./controllers')
+    .into(app)
+
+app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+
